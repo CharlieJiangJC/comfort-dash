92ecaad086ce067c36d361700e2e4f9b43fe22dc
jiadongZhangWorkIn <zjiadong53@gmail.com>
2024-10-17 23:10:12 +1100

    Changed input box text

--- pages/home.py
+++ pages/home.py
@@ -137,12 +137,19 @@ def update_store_inputs(
     Input(ElementsIDs.MODEL_SELECTION.value, "value"),
     Input(ElementsIDs.UNIT_TOGGLE.value, "checked"),
     Input(ElementsIDs.functionality_selection.value, "value"),
+    Input(ElementsIDs.chart_selected.value, "value"),  
+
 )
-def update_inputs(selected_model, units_selection, function_selection):
+def update_inputs(selected_model, units_selection, function_selection,chart_selected):
     if selected_model is None:
         return no_update
     units = UnitSystem.IP.value if units_selection else UnitSystem.SI.value
-    return (input_environmental_personal(selected_model, units, function_selection),)
+    if chart_selected == Charts.wind_temp_chart.value.name:
+        return input_environmental_personal(selected_model, units, function_selection, include_tr=False,is_operative_temperature=True)
+    elif chart_selected in [Charts.set_outputs.value.name, Charts.thl_psychrometric.value.name]:
+        return input_environmental_personal(selected_model, units, function_selection, include_tr=True,include_air_temp=False)
+    else:
+        return input_environmental_personal(selected_model, units, function_selection,include_tr=True,include_air_temp=True,is_operative_temperature=False)
 
 
 # once function: update_inputs via URL, update the value of the model dropdown, unit toggle and functionality dropdown and chart dropdown, and inputs, it only use once when the page is loaded
